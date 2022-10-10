a24ca989866a3ad0345e8c44a861ba8eea59994d
AndyBoltonMV <andrew.bolton@multiverse.io>
2022-10-10 14:17:33 +0100

    ref

---
+++ script.js
@@ -0,0 +1,19 @@
+const easy = document.querySelector("#e-bttn");
+const med = document.querySelector("#m-bttn");
+const hard = document.querySelector("#h-bttn");
+
+function shapes() {
+  //Easy challenge
+}
+
+function fizzBuzz() {
+  //Medium challenge
+}
+
+function taxCalc() {
+  //Hard challenge
+}
+
+easy.addEventListener("click", shapes);
+med.addEventListener("click", fizzBuzz);
+hard.addEventListener("click", taxCalc);
